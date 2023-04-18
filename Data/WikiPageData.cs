292db147a033b3c7d95ae4e4c4fa59be6cd91f97
Manu404 <contact@emmanuelistace.be>
2023-04-18 07:13:50 +0200

    add extracted datastrcture from wikipage and youtubeurl

---
+++ Data/WikiPageData.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiRef.Commons.Data
+{
+    public class WikiPageData
+    {
+        [JsonProperty] public string Name { get; set; }
+        [JsonProperty] public List<YoutubeUrlData> YoutubeUrls { get; set; }
+        [JsonProperty] public List<Reference> References { get; set; }
+        [JsonProperty] public string Content { get; set; }
+    }
+}
